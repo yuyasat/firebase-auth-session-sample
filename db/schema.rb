5446d0a336497542e20f76e390d125f8700d0141
yuyasat <takiyour@gmail.com>
2023-05-16 21:45:20 +0900

    add idaas_uid column to users

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_120059) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "idaas_uid", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
